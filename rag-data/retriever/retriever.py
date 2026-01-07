62676159d7d5207ef06e0b8c59dad9de3e027692
unknown <leafycraft6@gmail.com>
2026-01-07 13:06:51 +0800

    update retriever.py

--- rag-data/retriever/retriever.py
+++ rag-data/retriever/retriever.py
@@ -1,9 +1,39 @@
-import json 
 import faiss
-import numpy as np 
+import json
+import numpy as np
 from embedder import Embedder
-from pathlib import Path
-from intent_router import get_intent_priority_sections
+import os
 
-INDEX_PATH = Path("../embeddings/faiss_index/index.faiss")
-METADATA_PATH = Path("../embeddings/faiss_index/metadata.json")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+INDEX_DIR = os.path.join(BASE_DIR, "..", "embeddings", "faiss_index")
+
+INDEX_PATH = os.path.join(INDEX_DIR, "computer_service.index")
+META_PATH = os.path.join(INDEX_DIR, "computer_service_metadata.json")
+
+def load_faiss_index():
+    index = faiss.read_index(INDEX_PATH)
+    with open(META_PATH, "r", encoding="utf-8") as f:
+        metadata = json.load(f)
+    return index, metadata
+
+def search_query(query, top_k=3):
+    embedder = Embedder()
+    index, metadata = load_faiss_index()
+    
+    query_embedding = embedder.get_embedding(query)
+    query_embedding = np.expand_dims(query_embedding, axis=0)
+    
+    score, indices = index.search(query_embedding,top_k)
+    
+    results = []
+    for i in indices[0]:
+        results.append(metadata[i])
+    return results
+
+if __name__ == "__main__":
+    sample_query = "Layar laptop saya tiba-tiba menjadi hitam saat dinyalakan."
+    results = search_query(sample_query, top_k=3)
+    for i, res in enumerate(results):
+        print(f"Result {i+1}:")
+        print(json.dumps(res, indent=2, ensure_ascii=False))
+        print()
